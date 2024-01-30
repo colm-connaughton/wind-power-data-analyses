7f93b4dccac1ba223044758f3de30e8d269f50cc
Colm Connaughton <colm.connaughton@zonlab.com>
2024-01-30 11:16:41 +0000

    Tidying up variable names and moving the code to read dataset 1 into its own function.

--- functions.py
+++ functions.py
@@ -3,7 +3,8 @@ import os
 import yaml
 import logging
 import pathlib
-
+import pandas as pd
+import pickle
 
 
 def read_config(args):
@@ -30,3 +31,60 @@ def read_config(args):
       exit(3)
 
     return config
+
+def process_dataset_1(config):
+   # Process dataset 1 (low frequency data)
+
+    # Create a dictionary of dataframes for the windspeed data from each
+    # wind farm in the low frequency data files
+    windspeed={}
+
+    # For each windfarm in the low frequency data set extract the windspeed
+    # into a dataframe and standardise the column names, index etc
+    for windfarm_id, windfarm_name  in config["dataset1"]["windfarms"].items():
+        filename = windfarm_id.lower()+"_windspeed_COD_to_20230601.csv"
+        file = pathlib.Path(config["data_folder"], config["dataset1"]["subfolder"],
+                     config["dataset1"]["wind"]["subfolder"],
+                     filename)
+        if file.exists():
+          logging.info("Processing data file %s.", file)
+          # Read data into a dataframe and get the column names
+          windspeed[windfarm_id] = pd.read_csv(file, sep=",", header=0)
+          column_names = windspeed[windfarm_id].columns
+
+          # Different files have different labels for the observation time
+          # column. Rename them all to "time" and convert strings into datetime
+          # format
+          if "t_local" in column_names:
+            windspeed[windfarm_id]["t_local"] = pd.to_datetime(windspeed[windfarm_id]["t_local"], utc=True)
+            windspeed[windfarm_id].rename(columns={"t_local": "time"}, inplace=True)
+          elif "timestamp" in column_names:
+            windspeed[windfarm_id]["timestamp"] = pd.to_datetime(windspeed[windfarm_id]["timestamp"], utc=True)
+            windspeed[windfarm_id].rename(columns={"timestamp": "time"}, inplace=True)
+          else:
+            logging.error("No time column found in %s. Exiting.", file)
+            exit(4)
+
+          # Set the "time" column as the index
+          windspeed[windfarm_id].set_index("time", inplace=True)
+
+          # Drop the "Unnamed" column
+          if 'Unnamed: 0' in column_names:
+            windspeed[windfarm_id].drop(columns=['Unnamed: 0'], inplace=True)
+          logging.info(windspeed[windfarm_id].columns)
+
+          # Check if there are any NaNs and drop any rows containing NaNs
+          n = windspeed[windfarm_id].isnull().sum().sum()
+          if n>0:
+            logging.warning("%s NaNs found in %s.", n, file)
+            windspeed[windfarm_id].dropna(inplace=True, axis=0)
+
+          # Calculate the ensemble average for the wind farm
+          windspeed[windfarm_id]["Mean"] = windspeed[windfarm_id].mean(axis=1)
+
+          # Write the data to a new file
+          filename = "windspeed-"+windfarm_id.lower()+".pkl"
+          file = pathlib.Path(config["output_folder"], config["dataset1"]["subfolder"],
+                            filename)
+          with open(file, "wb") as f:
+            pickle.dump(windspeed[windfarm_id], f)
--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import sys
 import argparse
 import pathlib
 import logging
-import pandas as pd
-import pickle
 import functions
 
 
@@ -25,47 +23,8 @@ def main():
     # Read the passed config file
     config = functions.read_config(args)
 
-    locations = config["dataset1"]["locations"]
-    keys = config["dataset1"]["locations"].keys()
 
-    windspeed={}
-    for k in keys:
-        filename = k.lower()+"_windspeed_COD_to_20230601.csv"
-        file = pathlib.Path(config["data_folder"], config["dataset1"]["subfolder"],
-                     config["dataset1"]["wind"]["subfolder"],
-                     filename)
-        if file.exists():
-          logging.info("Processing data file %s.", file)
-          # Read data into a pandas dataframe
-          windspeed[k] = pd.read_csv(file, sep=",", header=0)
-          column_names = windspeed[k].columns
-          if "t_local" in column_names:
-            windspeed[k]["t_local"] = pd.to_datetime(windspeed[k]["t_local"], utc=True)
-            windspeed[k].rename(columns={"t_local": "time"}, inplace=True)
-          elif "timestamp" in column_names:
-            windspeed[k]["timestamp"] = pd.to_datetime(windspeed[k]["timestamp"], utc=True)
-            windspeed[k].rename(columns={"timestamp": "time"}, inplace=True)
-          else:
-            logging.error("No time column found in %s. Exiting.", file)
-            exit(4)
-          windspeed[k].set_index("time", inplace=True)
-          if 'Unnamed: 0' in column_names:
-            windspeed[k].drop(columns=['Unnamed: 0'], inplace=True)
-          logging.info(windspeed[k].columns)
-
-          # Check if there are any NaNs
-          n = windspeed[k].isnull().sum().sum()
-          if n>0:
-            logging.warning("%s NaNs found in %s.", n, file)
-            # Drop rows containing NaNs
-            windspeed[k].dropna(inplace=True, axis=0)
-
-          # Write the data to a new file
-          filename = "windspeed-"+k.lower()+".pkl"
-          file = pathlib.Path(config["output_folder"], config["dataset1"]["subfolder"],
-                            filename)
-          with open(file, "wb") as f:
-            pickle.dump(windspeed[k], f)
+    functions.process_dataset_1(config)
 
 
 
